1f66ea67b7ae40801c7d25682ef016838a1adc02
Vihar Parameswaran <vihar@cliqr.com>
2016-02-29 11:49:42 +0000

    importing manifests

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,41 @@
-node puppet-master {
-	file { '/tmp/hello':
-	  content => "Hello, world:\n",
+node 'puppet' {
+  include accounts
+  include firewall
+ } 
+
+node 'client1.novalocal' {
+  
+  include apache
+  include memcached 
+
+
+resources { 'firewall':
+    purge => true,
+  }
+
+  Firewall {
+    before        => Class['firewall::post'],
+    require       => Class['firewall::pre'],
+  }
+
+  class { ['firewall::pre', 'firewall::post']: }
+
 }
+        
+node 'client2.novalocal' {
+  include accounts
+
+  resources { 'firewall':
+    purge => true,
+  }
+
+  Firewall {
+    before        => Class['firewall::post'],
+    require       => Class['firewall::pre'],
+  }
+
+  class { ['firewall::pre', 'firewall::post']: }
+
 }
 
+ 
